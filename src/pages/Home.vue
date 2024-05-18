9b277493576ad6067e6355f80a0df39208ed3e79
Khian Abad <123443469+k3yan@users.noreply.github.com>
2024-05-18 14:15:19 +0800

    added card to home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,52 @@
 <template>
-  <div class="flex justify-start">Home</div>
+  <div class="flex justify-center">
+    <!--background design-->
+    <div
+      class="bg-purple-200 rounded-lg shadow-xl lg:w-[60rem] lg:h-[31rem] sm:w-[30rem] sm:h-[31rem]"
+    >
+      <!--Content-->
+      <div class="flex justify-between">
+        <!--Profile Config-->
+        <div
+          class="bg-purple-300 lg:h-[30rem] rounded-lg lg:w-[19rem] lg:ml-3 lg:mt-2 sm:w-[18rem] sm:h-[30rem] sm:ml-3 sm:mt-2 shadow-md"
+        >
+          <div
+            class="ml-12 mt-[5rem] rounded-full bg-slate-100 h-[10rem] w-[10rem]"
+          >
+            <img
+              src="../assets/me.jpg"
+              class="rounded-full my-2 mx-2 w-[10rem] h-[10rem] shadow-2xl"
+            />
+            <!--Labeling Config-->
+            <div class="mt-5">
+              <div class="flex justify-center text-xl">
+                Felicinart ₍ᐢ..ᐢ₎♡ -
+              </div>
+              <!--Card-->
+              <div
+                class="bg-purple-400 h-[10rem] w-[14rem] p-2 rounded-lg mt-3 shadow-lg text-gray-200"
+              >
+                <div class="">Now Playing....</div>
+                <div>what if pa commission kayo?</div>
+                <div>1:43 3:21 |───────●──────────|</div>
+                <div class="flex justify-evenly mt-4 cursor-pointer">
+                  <img src="../assets/pause.png" class="h-[1rem]" />
+                  <img src="../assets/backward.png" class="h-[1rem]" />
+                  <img src="../assets/play.png" class="h-[1rem]" />
+                  <img
+                    src="../assets/backward.png"
+                    class="h-[1rem] rotate-180"
+                  />
+                  <img src="../assets/hamburger.png" class="h-[1rem]" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>Content</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script></script>
