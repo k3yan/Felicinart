ebc433c5521a480f2491cfceaf2d80fbac2a350e
Khian Abad <123443469+k3yan@users.noreply.github.com>
2024-06-05 11:07:18 +0800

    added pricing page and modified the footer

--- src/pages/Pricing.vue
+++ src/pages/Pricing.vue
@@ -1,5 +1,35 @@
 <template>
-  <div>Pricing</div>
+  <div
+    class="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12 top-0"
+  >
+    <div class="mx-auto max-w-screen-xl px-4 w-full">
+      <div
+        class="-mt-[7rem] font-bold text-6xl text-fuchsia-200 flex justify-center italic font-dancing"
+      >
+        Service Pricing
+      </div>
+      <div
+        class="bg-fuchsia-300 dark:bg-purple-400 w-[15rem] mx-9 h-auto mt-9 mb-9 rounded-lg px-2 py-2"
+      >
+        <h1>✦ 1 - 7 days process</h1>
+        <h1>✦ paypal or gotyme payment</h1>
+        <h1>✦ discuss for complexity</h1>
+        <h1>✦ payments first</h1>
+      </div>
+      <div class="flex justify-center gap-9">
+        <div>
+          <img src="../assets/price.png" alt="art pricing" class="rounded-lg" />
+        </div>
+        <div>
+          <img
+            src="../assets/pricegrap.png"
+            alt="graphics pricing"
+            class="rounded-lg"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
